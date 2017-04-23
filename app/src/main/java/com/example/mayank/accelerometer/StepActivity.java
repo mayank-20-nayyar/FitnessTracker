f8cd1a29672b175b0237c8c231a5dc19834516e8
mowgli-m <mayanknayyar95@gmail.com>
2017-04-24 02:03:53 +0530

    added step_counter

---
+++ app/src/main/java/com/example/mayank/accelerometer/StepActivity.java
@@ -0,0 +1,139 @@
+package com.example.mayank.accelerometer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.DataReadRequest;
+
+import java.io.Console;
+import java.util.concurrent.TimeUnit;
+
+public class StepActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView textView;
+    private TextView textView2;
+
+    private SensorManager mSensorManager;
+
+    private Sensor mStepCounterSensor;
+
+    private Sensor mStepDetectorSensor;
+
+    public Sensor sensor;
+
+    int value = -1;
+    float[] values;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step);
+        textView = (TextView)findViewById(R.id.textview);
+        textView2 = (TextView)findViewById(R.id.textview2);
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+       /* DataSource ESTIMATED_STEP_DELTAS = new DataSource.Builder()
+                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .setType(DataSource.TYPE_DERIVED)
+                .setStreamName("estimated_steps")
+                .setAppPackageName("com.google.android.gms")
+                .build();
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(ESTIMATED_STEP_DELTAS, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();*/
+    }
+
+        /*count = (TextView) findViewById(R.id.count);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }*/
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        /*if (activityRunning) {
+            count.setText(String.valueOf(sensorEvent.values[0]));
+        }
+        count.setText(String.valueOf(sensorEvent.values[0]));
+        */
+        sensor = event.sensor;
+        values = event.values;
+
+
+        if (values.length > 0) {
+            value = (int) values[0];
+        }
+
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            textView.setText("Step Counter Detected : " + value);
+        } /*else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            // For test only. Only allowed value is 1.0 i.e. for step taken
+            textView.setText("Step Detector Detected : " + value);
+        }*/
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this, mStepCounterSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+
+        /*activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
+*/
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // if you unregister the last listener, the hardware will stop detecting step events
+//        sensorManager.unregisterListener(this);
+    }
+
+}
