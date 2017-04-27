366ec2f0d4568f720edf1a747634f681cea8dcf6
mowgli-m <mayanknayyar95@gmail.com>
2017-04-28 02:49:36 +0530

    added step counter as service

--- app/src/main/java/com/example/mayank/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/mayank/accelerometer/MainActivity.java
@@ -5,11 +5,23 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Mayank on 4/27/2017.
@@ -25,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
     SensorRestarterBroadcastReceiver receiver;
     public static final String BROADCAST = "com.example.mayank.service_broadcast_tester.android.action.broadcast";
 
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+
+    public double latitude;
+    public double longitude;
+
+
     public Context getCtx() {
         return ctx;
     }
@@ -38,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_step);
         textView = (TextView)findViewById(R.id.textview);
         textView.setText("walk");
+
+        displayLocation();
+
         BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -60,6 +82,57 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    String displayLocation() {
+
+        String currentAddress = "";
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "Please enable google play services";
+        }
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+
+            Log.e("the location", latitude + " " + longitude + " " );
+            currentAddress = convertToAddress(latitude, longitude);
+
+        } else {
+            Log.e("the location", "not possible");
+        }
+        return currentAddress;
+    }
+
+    String convertToAddress(double latitude, double longitude){
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName();
+            Log.e("the add",address + city + state + country + postalCode + knownName);
+            return address + " " + city + " " + state + " " + country + " " + postalCode + " " + knownName;// Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "some issue with finding the current location";
+    }
+
 
     private boolean isMyServiceRunning(Class<?> serviceClass) {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
